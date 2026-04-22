8b64310eb6ed51bb5fe1f623bacec2816203777b
kartik-2502 <kartikpal2502@gmail.com>
2026-04-22 15:55:10 +0000

    variables table updation

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 12322;
+let accountEmail = "Karrt@tgmail.com";
+var accountPassword = "1234";
+accountCity = "Seoul";
+
+accountEmail = "pal@new"
+accountPassword = "5678";
+accountCity = "Bangaluru"
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity]);
