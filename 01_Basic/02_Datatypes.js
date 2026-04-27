b6b78951f0a8e80d2d1b44e896e46674a0399489
kartik-2502 <kartikpal2502@gmail.com>
2026-04-27 18:14:38 +0000

    DATA TYPES

---
+++ 01_Basic/02_Datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all as nwe JS code as new standard
+
+// alert("Hello World!"); 
+// we are using and this function are used in the browsers
+
+console.log(3+3)
+
+let name = "Kartik";
+let age = 22;
+let isLoggedIn = false;
+let state;
+
+// number = 2 to power
+// bigint 
+// string 
+// boolean
+// null - standalone value
+// undefined - when the calue is not defined in the program 
+// symbol - unique identifier
+// object
+console.log(typeof undefined) // OP -> undefined
+console.log(typeof null) //  OP -> OBJECT this is a bug in JS but we have to live with it
