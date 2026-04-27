94c7128578e689bef41a0979b14a68ae33a39e53
kartik-2502 <kartikpal2502@gmail.com>
2026-04-27 18:37:07 +0000

    Conversion of the different dataypes into one another

---
+++ 01_Basic/03_conversionOperations.js
@@ -0,0 +1,24 @@
+// let score = "33abc"
+
+// console.log(typeof(score)) 
+// console.log(typeof score)   
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+//  "33abc" -> NaN
+// "33 " -> 33
+let isLoggedIn = null
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 - true ; 0 - false
+// " " - false ; "kartik" - true
+// null - false ; undefined - false
+
+let aNumber = 33
+let stringNumber = String(aNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
