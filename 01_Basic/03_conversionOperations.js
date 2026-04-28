627ba236cec79c55aaaf3d3d066f4378f22d2f1a
kartik-2502 <kartikpal2502@gmail.com>
2026-04-28 16:59:08 +0000

    more funtions

--- 01_Basic/03_conversionOperations.js
+++ 01_Basic/03_conversionOperations.js
@@ -1,24 +1,43 @@
-// let score = "33abc"
+// // let score = "33abc"
 
-// console.log(typeof(score)) 
-// console.log(typeof score)   
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber)
-// console.log(valueInNumber)
+// // console.log(typeof(score)) 
+// // console.log(typeof score)   
+// // let valueInNumber = Number(score)
+// // console.log(typeof valueInNumber)
+// // console.log(valueInNumber)
 
 
-//  "33abc" -> NaN
-// "33 " -> 33
-let isLoggedIn = null
+// //  "33abc" -> NaN
+// // "33 " -> 33
+// let isLoggedIn = null
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
-// 1 - true ; 0 - false
-// " " - false ; "kartik" - true
-// null - false ; undefined - false
+// // 1 - true ; 0 - false
+// // " " - false ; "kartik" - true
+// // null - false ; undefined - false
 
-let aNumber = 33
-let stringNumber = String(aNumber)
-console.log(typeof stringNumber)
-console.log(stringNumber)
+// let aNumber = 33
+// let stringNumber = String(aNumber)
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(typeof("2" + "2"))
+// console.log(2**2)
+
+// let str1 = "Kartik!!"
+// let str2 = "How are you?"
+// console.log(str1 + " " + str2)
+
+// console.log("2" + 2) // "22"
+// console.log(2 + 3 +"1")
+console.log(+"")
+console.log(+true)
+
+let num1 = num2  = num3 = 2+2
